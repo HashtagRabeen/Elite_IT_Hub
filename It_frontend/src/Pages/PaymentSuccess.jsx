b649ee021d7af0243216bc887a502f5af91ec8de
unknown <rabinstha74@gmail.com>
2025-05-08 16:36:33 +0545

    some changes

--- It_frontend/src/Pages/PaymentSuccess.jsx
+++ It_frontend/src/Pages/PaymentSuccess.jsx
@@ -38,6 +38,7 @@ function PaymentSuccess() {
     };
 
     if (user && parseData) recordPayment();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [parseData, user]);
 
   return (
